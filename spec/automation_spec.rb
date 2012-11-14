bb809ae50860ae480e40c9b646f266b47ad52d56
Evan Light <evan@tripledogdare.net>
2012-11-14 09:59:23 -0500

    Helps when the specs pass...

--- spec/automation_spec.rb
+++ spec/automation_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
-context "when #populate is not defined in the subclass" do
-  context "the default implementation" do
-    it "uses the value(s) passed to validate_and_save to locate the model params to use for population"
-    it "looks up the classified name for the symbol passes to validate_and_saves"
+describe Redtape::Form do
+  context "when #populate is not defined in the subclass" do
+    context "the default implementation" do
+      it "uses the value(s) passed to validate_and_save to locate the model params to use for population"
+      it "looks up the classified name for the symbol passes to validate_and_saves"
+    end
   end
 end
--- spec/nested_form_spec.rb
+++ spec/nested_form_spec.rb
@@ -1,34 +1,36 @@
 require 'spec_helper'
 
-context "simulating a nested form from the view for a User with many Addresses" do
-  context "where the Address form fields adhere to Address column names" do
-    subject { UserWithAddressesRegistrationForm.new(fake_rails_params) }
+describe Redtape::Form do
+  context "simulating a nested form from the view for a User with many Addresses" do
+    context "where the Address form fields adhere to Address column names" do
+      subject { UserWithAddressesRegistrationForm.new(fake_rails_params) }
 
-    let(:fake_rails_params) {
-      HashWithIndifferentAccess.new(
-        :first_name => "Evan",
-        :last_name => "Light",
-        :addresses_attributes => {
-          "0" => {
-            :address1 => "123 Foobar way",
-            :city     => "Foobar",
-            :state    => "MN",
-            :zipcode  => "12345"
-          },
-          "1" => {
-            :address1 => "124 Foobar way",
-            :city     => "Foobar",
-            :state    => "MN",
-            :zipcode  => "12345"
+      let(:fake_rails_params) {
+        HashWithIndifferentAccess.new(
+          :first_name => "Evan",
+          :last_name => "Light",
+          :addresses_attributes => {
+            "0" => {
+              :address1 => "123 Foobar way",
+              :city     => "Foobar",
+              :state    => "MN",
+              :zipcode  => "12345"
+            },
+            "1" => {
+              :address1 => "124 Foobar way",
+              :city     => "Foobar",
+              :state    => "MN",
+              :zipcode  => "12345"
+            }
           }
-        }
-      )
-    }
+        )
+      }
 
-    before do
-      subject.save
+      before do
+        subject.save
+      end
+
+      specify { subject.user.addresses.count.should == 2 }
     end
-
-    specify { subject.user.addresses.count.should == 2 }
   end
 end
