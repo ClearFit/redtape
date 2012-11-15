511c2077a7becd9b387c34c92d2ad61f570c8206
Evan Light <evan.light@tripledogdare.net>
2012-11-15 13:14:30 -0500

    This should still have been pending in master.  Oopsy.

--- spec/automation_spec.rb
+++ spec/automation_spec.rb
@@ -1,44 +1,10 @@
 require 'spec_helper'
 
 describe Redtape::Form do
-  subject { AutomatedRegistrationForm.new(fake_rails_params) }
-
   context "when #populate is not defined in the subclass" do
-    context "and the form provides some data that has not been whitelisted" do
-      let(:fake_rails_params) {
-        HashWithIndifferentAccess.new(
-          :name                   => "Evan Light",
-          :social_security_number => "123-456-7890",
-          :addresses_attributes   => {
-            "0" => {
-              :address1 =>   "123 Foobar way",
-              :city     =>   "Foobar",
-              :state    =>   "MN",
-              :zipcode  =>   "12345",
-              :alarm_code => "12345"
-            },
-            "1" => {
-              :address1 => "124 Foobar way",
-              :city     => "Foobar",
-              :state    => "MN",
-              :zipcode  => "12345",
-              :alarm_code => "12345"
-            }
-          }
-        )
-      }
-
-      before do
-        subject.save
-      end
-
-
+    context "the default implementation" do
       it "uses the value(s) passed to validate_and_save to locate the model params to use for population"
-      it "constantizes the symbol passed to validate_and_saves to identify the top level model class"
-      it "only records attributes on the model that have been whitelisted" do
-        subject.user.social_security_number.should be_nil
-        subject.addresses.each { |a| a.alarm_code.should be_nil }
-      end
+      it "looks up the classified name for the symbol passes to validate_and_saves"
     end
   end
 end
