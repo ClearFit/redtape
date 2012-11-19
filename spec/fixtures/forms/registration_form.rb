9e72ceb9815917b583ea447d671199330a396182
Evan Light <evan@tripledogdare.net>
2012-11-19 14:39:29 -0500

    Removed some dead code from the fixtures. Kinda important as they are API usage examples.

--- spec/fixtures/forms/registration_form.rb
+++ spec/fixtures/forms/registration_form.rb
@@ -1,8 +1,6 @@
 class RegistrationForm < Redtape::Form
   validates_and_saves :user
 
-  attr_accessor :first_name, :last_name
-
   def populate_individual_record(record, attrs)
     if record.is_a?(User)
       record.name = "#{attrs[:first_name]} #{attrs[:last_name]}"
--- spec/fixtures/forms/user_with_address_registration_form.rb
+++ spec/fixtures/forms/user_with_address_registration_form.rb
@@ -1,8 +1,6 @@
 require 'fixtures/forms/registration_form'
 
 class UserWithAddressesRegistrationForm < RegistrationForm
-  attr_accessor :addresses_attributes
-
   # NOTE: This handles *either* new records or updated records and not both at the
   # same time.  It's up to the Form subclass to decide how many cases it will support.
   # Your forms may not need more than just new or new-and-update.
